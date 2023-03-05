045bfff5db5c7fa8360bc45ec3528c8965c147f0
numangit <abdulnumanjob@gmail.com>
2023-03-05 22:17:58 +0600

    initial data structure created

---
+++ rtk/features/video/videoSlice.js
@@ -0,0 +1,14 @@
+//initial state
+const initialState = {
+    videoTags: [],
+    video: {
+        loading: false,
+        videoData: [],
+        error: ''
+    },
+    relatedVideos: {
+        loading: false,
+        videoData: [],
+        error: ''
+    }
+};
